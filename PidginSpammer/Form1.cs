f6c7998207fa56114ed31bcb661cfdbf7757f5c0
brad <avianbc@gmail.com>
2015-05-05 22:49:48 -0400

    First commit

---
+++ PidginSpammer/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace aim
+{
+    public partial class Form1 : Form
+    {
+        [return: MarshalAs(UnmanagedType.Bool)]
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern bool PostMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll")]
+        static extern uint MapVirtualKey(uint uCode, uint uMapType);
+
+        [DllImport("user32.dll")]
+        static extern short VkKeyScan(char ch);
+
+        public const Int32 WM_CHAR = 0x0102;
+        public const Int32 WM_KEYDOWN = 0x0100;
+        public const Int32 WM_KEYUP = 0x0101;
+        public const Int32 VK_RETURN = 0x0D;
+        public const Int32 VK_SHIFT = 0x10;
+        public const uint MAPVK_VK_TO_CHAR = 0x02;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Process[] proc = Process.GetProcessesByName("pidgin");
+
+            if (proc[0] == null || proc.Length == 0)
+            {
+                MessageBox.Show("Process not found.", "Aborting");
+                return;
+            }
+
+            if (textBox1.Text == String.Empty)
+                return;
+
+            foreach (string s in textBox1.Lines)
+            {
+                foreach (char c in s)
+                {
+                    //IntPtr val = new IntPtr(char.ToUpper(c));]
+                    IntPtr val = new IntPtr(VkKeyScan(c));
+
+                    //PostMessage(proc[0].MainWindowHandle, WM_KEYDOWN, new IntPtr(VK_SHIFT), new IntPtr(0));
+                    PostMessage(proc[0].MainWindowHandle, WM_KEYDOWN, val, new IntPtr(0));
+                    PostMessage(proc[0].MainWindowHandle, WM_CHAR, val, new IntPtr(0));
+                    PostMessage(proc[0].MainWindowHandle, WM_KEYUP, val, new IntPtr(0));
+                    //PostMessage(proc[0].MainWindowHandle, WM_KEYUP, new IntPtr(VK_SHIFT), new IntPtr(0));
+                }
+
+                PostMessage(proc[0].MainWindowHandle, WM_KEYDOWN, new IntPtr(VK_RETURN), new IntPtr(0));
+                PostMessage(proc[0].MainWindowHandle, WM_KEYUP, new IntPtr(VK_RETURN), new IntPtr(0));
+            }
+        }
+    }
+}
